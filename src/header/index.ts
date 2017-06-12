0d6514fe2c4f361cd428c6459eaed63c06ac1ade
Vladimir Sabev <vdsabev@gmail.com>
2017-06-12 18:48:00 +0300

    Fixed header spacing

--- src/header/index.ts
+++ src/header/index.ts
@@ -6,7 +6,7 @@ import * as firebase from 'firebase/app';
 import { route } from 'mithril';
 
 import { store } from '../store';
-import { User, isLoggedIn, canModerate } from '../user';
+import { User, isLoggedIn, canModerate, canAdmin } from '../user';
 
 // Header
 export const Header = () => {
@@ -22,7 +22,7 @@ export const Header = () => {
 
 // Logo
 const Logo = () => (
-  a({ oncreate: route.link, href: '/', id: 'logo', className: 'flex-row align-items-center', style: flex(1) }, [
+  a({ oncreate: route.link, href: '/', id: 'logo', className: 'flex-row align-items-center' }, [
     img({ src: 'logo.png' }),
     h1('Гората')
   ])
@@ -31,10 +31,14 @@ const Logo = () => (
 // Logged in
 const LoggedInHeader = (currentUser: User) => [
   canModerate(currentUser) ? div({ style: { 'margin-right': 'auto' } }, [
-    a({ oncreate: route.link, href: '/requests/new' }, '+ New Request'),
+    a({ oncreate: route.link, href: '/requests/new' }, '+ Нова Заявка'),
     br(),
-    a({ target: '_blank', href: `https://console.firebase.google.com/project/${process.env.FIREBASE_PROJECT_ID}/database/data` }, '+ Edit Data')
+    canAdmin(currentUser) ?
+      a({ target: '_blank', href: `https://console.firebase.google.com/project/${process.env.FIREBASE_PROJECT_ID}/database/data` }, '+ База Данни')
+      :
+      null
   ]) : null,
+  div({ style: flex(1) }),
   div({ className: 'text-right' }, [
     div(currentUser.auth.email),
     a({ onclick: logout }, 'Logout')
@@ -45,5 +49,6 @@ export const logout = () => firebase.auth().signOut().catch(console.error).then(
 
 // Logged out
 const LoggedOutHeader = (currentUser: User) => [
+  div({ style: flex(1) }),
   a({ oncreate: route.link, href: '/login' }, 'Login')
 ];
