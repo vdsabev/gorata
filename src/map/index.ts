c122dc53d3f2ab1230d6fd7f32bd54ab393abf62
Vladimir Sabev <vdsabev@gmail.com>
2017-06-12 20:27:38 +0300

    Add markers for requests

--- src/map/index.ts
+++ src/map/index.ts
@@ -1,13 +1,36 @@
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
 import { div, ComponentNode } from 'compote/html';
 import * as $script from 'scriptjs';
 
-let map: google.maps.Map;
-
 export const initializeMap = () => {
   $script(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`, () => {
-    map = new google.maps.Map(document.querySelector('#map'), {
+    const map = new google.maps.Map(document.querySelector('#map'), {
       zoom: 8,
       center: { lat: 42.765833, lng: 25.238611 }
+    });
+
+    const markers: Record<string, google.maps.Marker> = {};
+
+    const requestsRef = firebase.database().ref('requests');
+    requestsRef.off('child_added');
+
+    requestsRef.on('child_added', (requestChildSnapshot: any) => {
+      const request = { id: requestChildSnapshot.key, ...requestChildSnapshot.val() };
+      markers[request.id] = new google.maps.Marker({
+        position: request.geo,
+        map,
+        title: request.title
+      });
+    });
+
+    requestsRef.on('child_removed', (requestChildSnapshot: any) => {
+      const marker = markers[requestChildSnapshot.key];
+      if (marker) {
+        marker.setMap(null);
+        delete markers[requestChildSnapshot.key];
+      }
     });
   });
 };
