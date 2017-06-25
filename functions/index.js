0443917063c17a6858f1841d29cb17f23d42f5cc
Vladimir Sabev <vdsabev@gmail.com>
2017-06-26 01:22:18 +0300

    Move images from tmp to user folder after create

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const firebase = require('firebase-admin');
+const functions = require('firebase-functions');
+const path = require('path');
+const gcs = require('@google-cloud/storage');
+const url = require('url');
+const uuidv4 = require('uuid/v4');
+const env = require('var');
+
+firebase.initializeApp(functions.config().firebase);
+
+const storage = gcs({ projectId: env.FIREBASE_PROJECT_ID });
+const bucket = storage.bucket(env.FIREBASE_STORAGE_BUCKET);
+
+exports.moveImages = functions.database.ref('/requests/{requestId}/createdBy').onWrite((event) => {
+  const userId = event.data.val();
+
+  return new Promise((resolve, reject) => {
+    event.data.ref.parent.child('imageUrls').once('value').catch(reject).then((imageUrlsSnapshot) => {
+      const imageUrls = imageUrlsSnapshot.val();
+      if (!imageUrls) resolve();
+
+      Promise.all(
+        imageUrls.map((imageUrl, index) => new Promise((resolve, reject) => {
+          const imageUuid = url.parse(imageUrl).pathname.match(/.+tmp%2F([\w-]+)/)[1];
+          const imagePath = `tmp/${imageUuid}`;
+          const newImagePath = `users/${userId}/${uuidv4()}`;
+          bucket.file(imagePath).move(newImagePath).catch(reject).then(() => {
+            // https://cloud.google.com/storage/docs/collaboration#browser
+            const newImageUrl = `https://storage.cloud.google.com/${env.FIREBASE_STORAGE_BUCKET}/${newImagePath}`;
+            event.data.ref.parent.child(`imageUrls/${index}`).set(newImageUrl).catch(reject).then(resolve);
+          });
+        }))
+      ).catch(reject).then(resolve);
+    });
+  });
+});
